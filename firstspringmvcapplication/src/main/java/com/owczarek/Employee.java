1dc16609b69aea185d046b93cdc3f865f5d25295
Artur Owczarek <owczarek.artur@gmail.com>
2016-08-14 14:00:15 +0200

    Add first Spring MVC application

---
+++ firstspringmvcapplication/src/main/java/com/owczarek/Employee.java
@@ -0,0 +1,39 @@
+package com.owczarek;
+
+import java.math.BigDecimal;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private BigDecimal salary;
+
+    public Employee(String firstName, String lastName, BigDecimal salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+}
